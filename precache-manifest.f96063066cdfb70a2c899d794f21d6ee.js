16a1fb7c0ec058950a0192e9637cd5cd13444ebb
Perdikal <Alexis.perd@yahoo.com>
2020-08-05 02:30:24 +0300

    Updates

---
+++ precache-manifest.f96063066cdfb70a2c899d794f21d6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f01ca1043be0c44159f8aa30d3dddcb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7fd045cdb55e96c9528b",
+    "url": "/monsters-rolodex/static/css/main.3f7efe31.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd045cdb55e96c9528b",
+    "url": "/monsters-rolodex/static/js/main.e3e17c6b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
